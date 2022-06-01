e409ce5d033cd6e157070a5b3e0dc7bfd1e8ac68
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-06-01 20:18:29 +0200

    Time: 139 ms (10.05%), Space: 45.1 MB (74.72%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+ 
+
+    ans = nums.concat(nums) 
+    return ans
+ };
+ 
+ nums = [1,2,1];
+ getConcatenation(nums)
+
