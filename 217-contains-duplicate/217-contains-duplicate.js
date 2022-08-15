029efa8b33b31f912fc27e0da648f07991341dd8
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-08-15 21:54:29 +0200

    Time: 87 ms (95.56%), Space: 50.7 MB (63.20%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,13 +3,12 @@
  * @return {boolean}
  */
  var containsDuplicate = function(nums) {
-    nums  ;
+   
  
 
-    nums2 = [...new Set(nums)]
+    nums2  = [...new Set(nums)]
 
-    return (nums2.length < nums.length) ? true : false
-   
+    return  nums2.length < nums.length 
      
 };
 
