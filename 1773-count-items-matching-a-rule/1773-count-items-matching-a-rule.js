30f2da68a387788b7e404f0004c29da742a9e3aa
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-08-19 23:29:58 +0200

    Time: 138 ms (16.05%), Space: 45.2 MB (99.88%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
@@ -4,41 +4,29 @@
  * @param {string} ruleValue
  * @return {number}
  */
- var countMatches = function(items, ruleKey, ruleValue) {
-   let i =0 
+var countMatches = function (items, ruleKey, ruleValue) {
+    let i = 0
     items.forEach(element => {
-      if (ruleKey == "color")
-      {
-        if (element[1] == ruleValue )
-        {
-           i++
+        switch (ruleKey) {
+            case "color":
+                if (element[1] == ruleValue) { i++  }
+                break;
+            case "type":
+                if (element[0] == ruleValue) { i++ }
+                break
+            case "name":
+                if (element[2] == ruleValue) { i++  }
+                break
         }
-      }
-      if (ruleKey == "type")
-      {
-        if (element[0] == ruleValue )
-        {
-            i++
-        }
-      }
-      if (ruleKey == "name")
-      {
-        if (element[2] == ruleValue )
-        {
-            i++
-        }
-      }
+
+    });
 
 
-      
-});
-
-
-return i
+    return i
 };
 
 
-Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+Input: items = [["phone", "blue", "pixel"], ["computer", "silver", "phone"], ["phone", "gold", "iphone"]], ruleKey = "type", ruleValue = "phone"
 
 
 countMatches(items, ruleKey, ruleValue)
