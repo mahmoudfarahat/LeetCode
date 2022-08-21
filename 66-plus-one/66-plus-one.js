e1ba1f688b30f591282f077fe1ece34a9369f84a
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-08-21 13:04:56 +0200

    Time: 127 ms (5.35%), Space: 41.9 MB (63.66%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -8,8 +8,15 @@
    num2 = BigInt(digits.join(''))+BigInt(1)
  
 
-   
-var intArr = Array.from(String(num2), num => Number(num));
+ 
+ 
+var temp = ''+num2
+ 
+var intArr = [...temp].reduce((acc, n)=> acc.concat(+n), [] );
+     
+     
+     
+// var intArr = Array.from(String(num2), num => Number(num));
 
 return intArr
      
