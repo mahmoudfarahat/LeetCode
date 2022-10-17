103ac90643ca7ed2f1a705079195baccfc43a972
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-10-17 14:10:58 +0200

    Time: 125 ms (47.87%), Space: 47.4 MB (27.45%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -17,7 +17,13 @@ for(let i = 0 ; i < nums.length ; ++i)
     }
 }
 
-   return Object.keys(obj).find(key => obj[key] === 1);
+for (let key in obj)
+{
+    if (obj[key] === 1) 
+    {
+       return key 
+    }
+}
  
 
 
