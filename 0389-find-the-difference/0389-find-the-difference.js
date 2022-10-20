87edfa576a72518e09011f463e9fb0cc9aaa42e4
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-10-20 15:36:53 +0200

    Time: 122 ms (34.25%), Space: 46.3 MB (12.88%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+ var findTheDifference = function(s, t) {
+   t ;
+    for(i = 0 ; i < s.length ; ++i)
+    {
+
+   var t = t.replace(s[i],'');
+
+    }
+
+   return t  //prints: 123
+
+};
