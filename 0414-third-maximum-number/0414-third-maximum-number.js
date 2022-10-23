c649f32b97d6ec169b8a685b75f79a7544dfd6cc
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-10-23 11:02:33 +0200

    Time: 99 ms (61.87%), Space: 44.4 MB (40.34%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+  
+ arr = nums.sort((a,b) => a-b).reverse()
+  const set = new Set(arr);
+  const array = Array.from(set);
+   
+   if(array[2] || array[2] == 0){
+    return array[2]
+  }else{
+  return Math.max(...array)
+  }
+};
+
+
+
