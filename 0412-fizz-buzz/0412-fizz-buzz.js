a18e5ac866488f1eb5001f73dd776c842bf20755
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-10-26 15:47:32 +0200

    Time: 134 ms (8.08%), Space: 44.7 MB (21.37%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,24 @@
+var fizzBuzz = function(n) {
+    arr = []
+
+    for (let i = 1; i < n+1; i++) {
+        if(i % 3 == 0 && i % 5 == 0 )
+        {
+            arr.push("FizzBuzz")
+        }else if(i % 3 == 0   ){
+            arr.push("Fizz")
+            
+        }else if(i % 5 == 0){
+            arr.push("Buzz")
+            
+        }else{
+            arr.push(i.toString())
+        }
+         
+         
+    }
+
+    
+return arr
+    
+};
