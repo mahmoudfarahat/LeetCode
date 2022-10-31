04e06c2b532fa0db90f6a11045a51eb1499494c1
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-10-31 09:15:31 +0200

    Time: 207 ms (37.29%), Space: 48.2 MB (45.95%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+var addToArrayForm = function(num, k) {
+         return  (BigInt(num.join(""))+BigInt(k)).toString().split("").map(a =>  +a)
+
+};
