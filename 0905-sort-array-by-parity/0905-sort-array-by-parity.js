7619bae7f2e4d7145d20f9e3f4a94f087c93c203
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-11-01 11:26:44 +0200

    Time: 89 ms (86.57%), Space: 44.7 MB (43.71%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+arr =[]
+ for (let i = 0; i < nums.length; i++) {
+   if(nums[i] % 2 == 0){
+    arr.unshift(nums[i])
+   }else{
+    arr.push(nums[i])
+    
+   }
+  
+ }
+
+ return arr
+};
