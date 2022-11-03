4abb68fbebf66ece395195f7beda8e673db5a560
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-11-03 09:56:50 +0200

    Time: 82 ms (85.86%), Space: 43.9 MB (41.41%) - LeetHub

---
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numDifferentIntegers = function(word) {
+
+ 
+ newWord =  word.replace(/[a-z]/gi," ").trim()
+ 
+ while (newWord.includes("  ")) {
+  
+  newWord =  newWord.replaceAll("  "," ")
+  
+ }
+    
+    if(newWord == '')
+        {return 0}
+
+newWord = [...new Set(newWord.split(" "))]
+ 
+ for (let i = 0; i < newWord.length; i++) {
+  
+ newWord[i] = newWord[i].replace(/^0+/g,"")
+ 
+}
+
+ return [...new Set (newWord)].length
+};
