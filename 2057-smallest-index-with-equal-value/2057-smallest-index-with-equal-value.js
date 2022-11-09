ac0ec5fa72864005a72f48afd32ac699a939bf84
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-11-09 11:55:37 +0200

    Time: 70 ms (96.28%), Space: 43.4 MB (97.34%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var smallestEqual = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+     if( nums[i] === i % 10 )
+     {
+      return i
+     }
+    }
+    return -1
+};
