130b86194d5182e610f50d53e4e92ac753a81493
Mahmoud Farahat <73858621+mahmoudfarahat@users.noreply.github.com>
2022-11-13 15:47:55 +0200

    Time: 109 ms (47.32%), Space: 42.5 MB (94.48%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+    marks = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+ p = 0
+arr =[ ]
+
+ for (let i = 0; i < words.length; i++) {
+  p = 0
+  newFormat =''
+  for (let j = 0; j < words[i].length; j++) {
+newFormat += marks[alphabet.indexOf(words[i][j])]
+ 
+++p
+   if(p ==  words[i].length){
+    arr.push(newFormat)
+   }
+  }
+ }
+return [...new Set(arr)].length
+};
